859c048b8a2a6b491d7f00aef107dc58b4c70da3
Rakib140 <69393935+Rakib140@users.noreply.github.com>
2020-08-08 23:32:08 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Bx Slider Start
+
+
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    jQuery('#main_menu').meanmenu({
+        meanMenuContainer:'#mobile_menu',
+        meanScreenWidth:991,
+    });
+  });
+
+
+
+
+//Bx Slider End
